266874a2dfc28f255c448097225be040903bcdb3
iMoloy <moloykrishnapaul@gmail.com>
2026-05-19 19:44:14 +0600

    Font changed to Inter+Outfit

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,3 +1,4 @@
+import { Inter, Outfit } from "next/font/google";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import "./globals.css";
@@ -5,21 +6,18 @@ import { AppProviders } from "@/providers/AppProviders";
 import { Footer } from "@/components/Footer";
 import { Navbar } from "@/components/Navbar";
 
+const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
+const outfit = Outfit({ subsets: ["latin"], variable: "--font-outfit" });
+
 export const metadata = {
   title: "AxleWay",
-  description: "A clean car rental platform for assignment CAT_05"
+  description: "A premium car rental platform for assignment CAT_05"
 };
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" suppressHydrationWarning>
-      <head>
-        <link
-          href="https://api.fontshare.com/v2/css?f[]=pilcrow-rounded@400&f[]=roundo@500&display=swap"
-          rel="stylesheet"
-        />
-      </head>
-      <body suppressHydrationWarning>
+    <html lang="en" suppressHydrationWarning className={`${inter.variable} ${outfit.variable}`}>
+      <body className="antialiased" suppressHydrationWarning>
         <AppProviders>
           <div className="page-shell">
             <Navbar />
